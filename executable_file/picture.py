5fc1d7232ed10c23c8d53af4d32f2f272ebe972c
yycheng <jasonyang092@gmail.com>
2023-10-07 15:17:17 +0800

    picture.0.0.1

--- executable_file/picture.py
+++ executable_file/picture.py
@@ -1,6 +1,7 @@
 from views import *
-import cv2
+import cv2  # pip install opencv-python
 
-image = cv2.imread('C:/Venv/Project/Kevin_mediapipe/mediapipe/img/wei_c200cm_h10cm.jpg')
-pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.3, model_complexity=2)
-detectPose(image, pose, display=True)
+image = cv2.imread('C:/Venv/Project/Kevin_mediapipe/mediapipe/img/wei_c200cm_h10cm.jpg') # 讀取圖片(路徑)
+#姿勢估計對象為pose，估計靜態圖片，最小的檢測信賴區間為0.3，模型複雜度為2
+pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.3, model_complexity=2) 
+detectPose(image, pose, display=True)   #後製圖片，顯示參數在圖片上

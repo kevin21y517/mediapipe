f9891a05af29b9904cedb3814e2ead9113edeb8f
yycheng <jasonyang092@gmail.com>
2023-10-07 15:00:48 +0800

    v.0.0.1

--- executable_file/main.py
+++ executable_file/main.py
@@ -1,31 +1,27 @@
 from views import *
-import cv2
+import cv2   # pip install opencv-python  
 
+cap = cv2.VideoCapture(0)  # 打開默認攝像頭
 
-if __name__ == "__main__":
-    cap = cv2.VideoCapture(0 )
+# 檢查攝像頭是否打開
+if not cap.isOpened():
+        print("無法打開攝像頭，請檢查是否已連接攝像頭。")
+else:
+    while True:
+        success, image = cap.read() # 如果成功讀取攝像頭的一幀影象
+        if not success: 
+            print("無法讀取攝像頭影象。")
+            break
+        cv2.imshow('Camera', image) # 顯示攝像頭影象，窗口名稱為Camera
+        # 如果按下空格键，则执行姿势估计
 
-    # 检查摄像头是否成功打开
-    if not cap.isOpened():
-        print("无法打开摄像头。请检查连接或驱动程序。")
-    else:
-        while True:
-            success, image = cap.read()
-            if not success:
-                print("无法接收帧。")
+        if cv2.waitKey(1) == 32:  # 空格键的ASCII码是32
+            run_time=300
+            cv2.destroyWindow('Camera') # 關閉攝像頭視窗
+            process_pose_estimation(cap, run_time)  #估計姿勢，傳遞參數
+            if cv2.waitKey(1) == 27: # 按下ESC键退出
                 break
-
-            # 在窗口中显示帧
-            cv2.imshow('Camera', image)
-
-            # 如果按下空格键，则执行姿势估计
-            if cv2.waitKey(1) == 32:  # 空格键的ASCII码是32
-                run_time=300
-                cv2.destroyWindow('Camera')
-                process_pose_estimation(cap, run_time)
-            if cv2.waitKey(1) == 27:
-                break
-
-        # 释放摄像头并关闭窗口
-        cap.release()
-        cv2.destroyAllWindows()
+    
+    # 釋放資源，關閉攝像頭和視窗
+    cap.release() 
+    cv2.destroyAllWindows()
